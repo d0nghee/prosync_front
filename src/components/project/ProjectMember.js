9fb2f6261e89f6617f9c1b5890aa9aff9ba8ff6d
kangwook <rlarkddnr1686@naver.com>
2023-10-22 14:26:19 +0900

    refactor: loader 인증 오류 해결

--- src/components/project/ProjectMember.js
+++ src/components/project/ProjectMember.js
@@ -149,7 +149,8 @@ export default function ProjectMember({ members, projectId }) {
     const invitePostSuccess = (response) => {
       console.log('invitePostSuccess');
       const inviteCode = response.data.data.inviteCode;
-      setInviteLink(`http://13.209.219.235:8080/projects/invite/${inviteCode}`);
+      // setInviteLink(`http://13.209.219.235:8080/projects/invite/${inviteCode}`);
+      setInviteLink(`http://prosyncfront.s3-website.ap-northeast-2.amazonaws.com/projects/invite/${inviteCode}`);
       // setInviteLink(`http://localhost:3000/projects/invite/${inviteCode}`);
       setIsModalOpen(true);
       setIsLoading(false);
