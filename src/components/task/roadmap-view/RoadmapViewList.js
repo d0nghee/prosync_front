b179bad6211bed1bbbf57517d8cdb3050fa20728
ShimChang <95128916+ShimChang@users.noreply.github.com>
2023-10-22 14:21:53 +0900

    feat/project 로드맵뷰 현재 날짜 포커스 수정

--- src/components/task/roadmap-view/RoadmapViewList.js
+++ src/components/task/roadmap-view/RoadmapViewList.js
@@ -1,117 +1,3 @@
-// import React, { useEffect, useRef } from 'react';
-// import { Timeline, DataSet } from 'vis-timeline/standalone';
-// import styled from 'styled-components';
-// import { useSelector } from 'react-redux';
-// import Guidance from '../../common/Guidance';
-// import ListLoadingSpinner from '../../common/ListLoadingSpinner';
-// import { useParams, useSearchParams } from 'react-router-dom';
-
-// export default function RoadmapViewList() {
-//   const timelineRef = useRef(); // 하나의 Timeline에 대한 ref를 저장할 변수
-//   const tasks = useSelector((state) => state.taskList.list);
-//   const taskStatusList = useSelector((state) => state.taskStatus.list);
-//   const pageInfo = useSelector((state) => state.taskList.pageInfo);
-//   const [searchParams] = useSearchParams();
-//   const params = useParams();
-//   const view = searchParams.get('view');
-
-//   useEffect(() => {
-//     if (tasks.length !== 0 && tasks[0].startDate) {
-//       const timelineData = tasks.map((task) => {
-//         let endDate = task.endDate;
-//         if (task.startDate === task.endDate) {
-//           let end = new Date(task.endDate);
-//           end.setDate(end.getDate() + 1);
-//           endDate = end.toISOString().slice(0, 10);
-//         }
-//         return {
-//           id: task.taskId,
-//           content: task.title,
-//           start: task.startDate,
-//           end: endDate,
-//           style: `background-color: ${task.color}; color: #fff; border-radius: 4px;`,
-//         };
-//       });
-
-//       const items = new DataSet(timelineData);
-
-//       //TODO: 최소 1일 최대 한달
-//       const options = {
-//         zoomMin: 1000 * 60 * 60 * 24 * 30, // 최소 줌 1일
-//         zoomMax: 1000 * 60 * 60 * 24 * 30,
-//         height: '700px',
-//         timeAxis: { scale: 'day', step: 1 },
-//       };
-
-//       new Timeline(timelineRef.current, items, null, options);
-//     }
-//   }, [tasks, taskStatusList, params.projectId, view]);
-
-//   if (!pageInfo.hasOwnProperty('page')) {
-//     return <ListLoadingSpinner />;
-//   } else if (pageInfo.totalElements === 0) {
-//     return <Guidance text="업무가 존재하지 않습니다." />;
-//   }
-
-//   return (
-//     <Container>
-//       <StatusLegendContainer>
-//         {/* status 색 보여주는 화면 */}
-//         {taskStatusList &&
-//           taskStatusList.map(({ taskStatusId, taskStatus, color }) => (
-//             <StatusLegend key={taskStatusId} color={color}>
-//               {taskStatus}
-//             </StatusLegend>
-//           ))}
-//       </StatusLegendContainer>
-//       <TimelineContainer>
-//         <Header>RoadMap</Header>
-//         <StyledTimeline ref={timelineRef} />
-//       </TimelineContainer>
-//     </Container>
-//   );
-// }
-
-// const Container = styled.div`
-//   width: 100%;
-//   padding: 20px;
-//   background-color: #f4f4f4;
-// `;
-
-// const TimelineContainer = styled.div`
-//   background-color: #ffffff;
-//   padding: 20px;
-//   border-radius: 10px;
-//   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-// `;
-
-// const Header = styled.h2`
-//   font-size: 20px;
-//   margin-bottom: 20px;
-//   color: #333;
-// `;
-
-// const StyledTimeline = styled.div`
-//   width: 100%;
-//   height: 700px;
-// `;
-// const StatusLegendContainer = styled.div`
-//   display: flex;
-//   flex-wrap: wrap;
-//   margin-bottom: 20px;
-// `;
-
-// const StatusLegend = styled.div`
-//   background-color: ${({ color }) => color};
-//   color: #fff;
-//   padding: 5px 10px;
-//   margin-right: 10px;
-//   margin-bottom: 10px;
-//   border-radius: 4px;
-//   font-size: 14px;
-// `;
-
-
 import React, { useEffect, useRef } from 'react';
 import { Timeline, DataSet } from 'vis-timeline/standalone';
 import styled from 'styled-components';
@@ -139,6 +25,7 @@ export default function RoadmapViewList() {
           end.setDate(end.getDate() + 1);
           endDate = end.toISOString().slice(0, 10);
         }
+
         return {
           id: task.taskId,
           content: task.title,
@@ -160,11 +47,14 @@ export default function RoadmapViewList() {
 
       const timeline = new Timeline(timelineRef.current, items, null, options);
 
-      timeline.on('select', function(properties) {
+      const currentDate = new Date();
+      timeline.setWindow(currentDate, currentDate);
+
+      timeline.on('select', function (properties) {
         if (properties.items.length > 0) {
           const selectedItemId = properties.items[0]; // 선택된 아이템의 ID를 가져옵니다.
           const selectedItem = items.get(selectedItemId); // ID를 사용하여 해당 아이템의 데이터를 가져옵니다.
-          console.log("Selected item:", selectedItem);
+          console.log('Selected item:', selectedItem);
           navigate(`/projects/${params.projectId}/tasks/${selectedItem.id}`);
         }
       });
